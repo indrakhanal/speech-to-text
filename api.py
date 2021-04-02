399adcb3d912bced8cdaaa483edb9fd38b26efd6
Indra Khanal <37183477+indrakhanal@users.noreply.github.com>
2021-04-02 11:21:18 +0545

    added procfile

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 #     try:
 #         # print("A moment of silence, please...")
 #         with m as source: r.adjust_for_ambient_noise(source)
-#         print("Set minimum energy threshold to {}".format(r.energy_threshold))
+#         print("Set minimum energy threshold to {}".format(r.energy_threshold))python
 #         print("Say something!")
 #         with m as source:
 #             audio = r.listen(source)
