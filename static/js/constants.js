5e9498fa194a90ceb4147d56775ed10e3d218ffd
Indra Khanal <37183477+indrakhanal@users.noreply.github.com>
2021-04-04 15:50:51 +0545

    hello

--- static/js/constants.js
+++
@@ -1,4 +0,0 @@
-const action_name = "action_greet";
-const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
-const sender_id = uuidv4();
-const flask_server_url = "http://127.0.0.1:5000/speak/";
